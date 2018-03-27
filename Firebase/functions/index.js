f019794cb8a4f56ccf4353a9a79d9afe6a4b1f24
inglec <inglec@tcd.ie>
2018-03-27 19:00:59 +0100

    finished back end, implented browserify

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -8,8 +8,8 @@ const cryptoJS   = require('crypto-js'); // https://www.npmjs.com/package/crypto
 const rsa        = require('node-rsa'); // https://github.com/rzcoder/node-rsa
 
 // Encryption Keys
-const rsaKey = new rsa(fs.readFileSync('keys/server-private-key.pem', 'utf8'));
-const aesKey = fs.readFileSync('keys/server-symmetric-key.txt', 'utf8');
+const rsaKey = new rsa(functions.config().keys.serverprivatekey);
+const aesKey = functions.config().keys.serversymmetrickey;
 
 // var key = new rsa().generateKeyPair(2048, 65537); // Generate 2048-bit key
 // var publicKey  = key.exportKey('public'); // 'pkcs8-public-pem'
@@ -26,35 +26,33 @@ const redditTypes = {
     'award':     't6'
 };
 
-/*
- * users = {
- *     inglec: {
- *         hashedPassword: "string",
- *         publicKey: "string"
- *     }
- * }
- */
- var users = {};
+var users = {
+    // Test user
+    inglec: {
+        // password: "helloworld",
+        hashedPassword: '1594244d52f2d8c12b142bb61f47bc2eaf503d6d9ca8480cae9fcf112f66e4967dc5e8fa98285e36db8af1b8ffa8b84cb15e0fbcf836c3deb803c13f37659a60',
+        publicKey: '-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAinwIoZMhTT57dgjBzJSr6BpP9D1KYdOXot7XApjLN3WH1j5f7Tm31fc6f3wYP514iX8tOH+1M5GSdv9rtXACU+jsAThaUngtM+bNcOCsGlyft6hUT79UnHINb9JwMI1CVnSBgk7UtCwaaYf7g0aOUbbFeUcwd+ZxiBdeZR58IuOPRZ1FG+ZzUntt/pM8ARI1m820Pbu/5W+CP8JyawC1nHG+SSZmlq8gD7D6pNRJlusTyeoq/J83lgN6SjK5rN4B8kdNmEMdNUPACh5wqigrjAwVDIraG2HmiTvb5A90LYJM+q3c9UWzgolodJeRI5baFGAFROHzf6QPdIrA0wBVNwIDAQAB-----END PUBLIC KEY-----'
+    }
+};
 
-/*
- * groups = {
- *     1723847: {
- *         users: {
- *             inglec: true
- *         }
- *     }
- * }
- */
- var groups = {};
+var groups = {
+    // Test groups
+    'group 18': {
+        users: {
+            inglec: true
+        }
+    }
+};
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/register', function(request, response) {
     // Decrypt body of request using RSA.
-    var body = JSON.parse(
-        rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
-    );
+    //var body = JSON.parse(
+    //    rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
+    //);
+    var body = request.body // TODO remove
 
     var user = users[body.username];
     if (user) {
@@ -63,7 +61,9 @@ app.post('/register', function(request, response) {
     }
     else {
         // Store new user
-        user.hashedPassword = cryptoJS.SHA512(body.password).toString();
+        users[body.username] = {
+            hashedPassword: cryptoJS.SHA512(body.password).toString()
+        }
 
         response.status(200);
         response.send('Success');
@@ -72,9 +72,10 @@ app.post('/register', function(request, response) {
 
 app.post('/login', function(request, response) {
     // Decrypt body of request using RSA.
-    var body = JSON.parse(
-        rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
-    );
+    //var body = JSON.parse(
+    //    rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
+    //);
+    var body = request.body // TODO remove
 
     var user = users[body.username];
     if (user) {
@@ -96,37 +97,24 @@ app.post('/login', function(request, response) {
     }
 });
 
-/*
- * request.body = {
- *     encryptedContent: "RSA-encrypted string"
- * }
- *
- * body = {
- *     username: "user",
- *     post: {
- *         title: "Post Title"
- *         content: "This is a test post",
- *         group: "groupID"
- *     }
- * }
- */
 app.post('/encrypt', function(request, response) {
     // Decrypt body of request using RSA.
-    var body = JSON.parse(
-        rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
-    );
+    //var body = JSON.parse(
+    //    rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
+    //);
+    var body = request.body // TODO remove
 
     var user = users[body.username];
     if (user) {
         // Check if user is a member of the group being posted to.
         var group = groups[body.post.group];
         if (group && group.users[body.username]) {
-            // Encrypt post with AES
+            // Encrypt post with AES.
             var encryptedPost = encryptPost(body.post);
 
-            // Encrypt encrypted post with user's RSA public key.
-            var userKey = new rsa(user.publicKey, 'utf8');
-            var encryptedResponse = userKey.encrypt(encryptPost);
+            // Encrypt post with users RSA public key in base 64.
+            var userKey = new rsa(user.publicKey);
+            var encryptedResponse = userKey.encrypt(new Buffer(JSON.stringify(encryptedPost)), 'base64');
 
             response.status(200);
             response.json({
@@ -144,42 +132,42 @@ app.post('/encrypt', function(request, response) {
     }
 });
 
-/*
- * request.body = {
- *     encryptedContent: "RSA-encrypted string"
- * }
- *
- * body = {
- *     username: "user",
- *     url: "https://www.reddit.com/r/tcdcrypto/comments/85u0p5/hai_unencrypted/"
- * }
- */
+function encryptPost(post) {
+    var encryptedPostContent = cryptoJS.AES.encrypt(JSON.stringify(post), aesKey).toString();
+    var encryptedPost = {
+        title: '#inglec-crypto:' + post.group,
+        content: encryptedPostContent
+    };
+    return encryptedPost;
+}
+
 app.post('/decrypt', function(request, response) {
     // Decrypt body of request using RSA.
-    var body = JSON.parse(
-        rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
-    );
+    //var body = JSON.parse(
+    //    rsaKey.decrypt(new Buffer(request.body.encryptedContent, 'base64'), 'utf8')
+    //);
+    var body = request.body // TODO remove
 
-    var user = users[username];
+    var user = users[body.username];
     if (user) {
         // Request Reddit post
         var url = require('url').parse(body.url);
         var options = {
             method: 'GET',
             hostname: url.hostname,
-            path: url.pathname + '.json',
+            path: url.pathname + '.json', // Return JSON response.
             port: 443
         };
         https.request(options, function(redditResponse) {
-            if (redditResponse.statusCode === 200) {
+            if (redditResponse.statusCode === 200) { // OK
                 var buffer = [];
                 redditResponse.on('data', function(data) {
                     buffer.push(data);
                 });
                 redditResponse.on('end', function() {
                     buffer = Buffer.concat(buffer);
-                    var json = JSON.parse(buffer.toString());
-                    var post = decryptPost(json);
+                    var encryptedPost = JSON.parse(buffer.toString());
+                    var post = decryptPost(encryptedPost);
 
                     var group = groups[post.group];
                     if (group.users[body.username]) {
@@ -202,7 +190,7 @@ app.post('/decrypt', function(request, response) {
                 response.status(redditResponse.statusCode);
                 response.send(redditResponse.statusMessage);
             }
-        }).end();
+        }).end(); // Send HTTPS request.
     }
     else {
         response.status(401);
@@ -210,48 +198,20 @@ app.post('/decrypt', function(request, response) {
     }
 });
 
-function encryptPost(post, group) {
-    // TODO
-
-    return JSON.stringify({});
-}
-
-/*
- * returns: {
- *     group: "groupID",
- *     title: "Title",
- *     content: "This is an unencrypted post."
- * }
- */
-function decryptPost(json) {
-    // TODO
-
-    /*
-    var encrypted = cryptoJS.AES.encrypt('hello', key).toString();
-    var decrypted = cryptoJS.AES.decrypt(encrypted, key).toString(cryptoJS.enc.Utf8);
-    */
-
-    // Get post
-    var post = json[0].data.children[0];
-    var comments = json[1].data.children;
-
-    var title = post.data.title;
-    var text = post.data.selftext;
+function decryptPost(encryptedPost) {
+    var post = encryptedPost[0].data.children[0];
+    
+    var group = post.data.title.split('#inglec-crypto:')[1];
+    var encryptedContent = post.data.selftext;
+    var decryptedPost = JSON.parse(
+        cryptoJS.AES.decrypt(encryptedContent, aesKey).toString(cryptoJS.enc.Utf8)
+    );
 
     return {
-        group: "groupID",
-        title: post.data.title,
-        content: post.data.selftext
+        group: group,
+        title: decryptedPost.title,
+        content: decryptedPost.content
     };
-}
-
-function desanitiseHtml(html) {
-    html = html.replace(/&gt;/g, '>');
-    html = html.replace(/&lt;/g, '<');
-    html = html.replace(/&quot;/g, '"');
-    html = html.replace(/&apos;/g, "'");
-    html = html.replace(/&amp;/g, '&');
-    return html;
 }
 
 exports.app = functions.https.onRequest(app);
